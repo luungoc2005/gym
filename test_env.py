6c46074bc754b12026d68bf27627c128206f0c76
luungoc2005 <luungoc2005@gmail.com>
2020-06-19 02:15:12 +0800

    WIP pipe communication

--- test_env.py
+++ test_env.py
@@ -8,7 +8,7 @@ from stable_baselines.common.policies import MlpLstmPolicy, MlpLnLstmPolicy, Mlp
 # from stable_baselines.sac.policies import MlpPolicy, LnMlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv, VecNormalize, SubprocVecEnv
 from stable_baselines.common.callbacks import BaseCallback
-from stable_baselines import PPO2, SAC
+from stable_baselines import PPO2, SAC, ACKTR
 
 from gym.envs.zipline.zipline_env import ZiplineEnv
 
@@ -29,7 +29,7 @@ class CheckpointCallback(BaseCallback):
     def _on_step(self) -> bool:
         if self.n_calls % self.save_freq == 0:
             path = os.path.join(self.save_path, '{}_{}_steps'.format(self.name_prefix, self.num_timesteps))
-            vec_norm_path = os.path.join(self.save_path, '{}_{}_steps_norm'.format(self.name_prefix, self.num_timesteps))
+            vec_norm_path = os.path.join(self.save_path, '{}_{}_steps_norm.pkl'.format(self.name_prefix, self.num_timesteps))
             self.model.save(path)
             if self.vec_norm is not None:
                 self.vec_norm.save(vec_norm_path)
@@ -37,31 +37,25 @@ class CheckpointCallback(BaseCallback):
                 print("Saving model checkpoint to {}".format(path))
         return True
 
-SAVE_PATH = "sac_zipline_sortino_lookback_21"
-STATS_PATH = "sac_zipline_sortino_lookback_21_norm.pkl"
+SAVE_PATH = "ppo2_zipline_sortino_lookback_21"
+STATS_PATH = "ppo2_zipline_sortino_lookback_21_norm.pkl"
 
 ENV_ARGS = {
     "start_date": "2018-12-1",
     "end_date": "2020-5-24",
     "lookback_window": 21,
+    "do_normalize": False,
+    "communication_mode": "redis"
 }
+TRAIN_STEPS = 280
 N_ENVS = 4
 DO_TRAIN = True
 
 if __name__ == "__main__":
     if DO_TRAIN:
-        env = SubprocVecEnv([lambda: ZiplineEnv(**ENV_ARGS, max_steps=256)] * N_ENVS)
-        # env = VecNormalize(env, norm_obs=True, norm_reward=True,
-        #     clip_obs=10.)
-
-        # model = PPO2(
-        #     MlpLstmPolicy, 
-        #     env,
-        #     verbose=1, 
-        #     n_steps=256,
-        #     nminibatches=N_ENVS,
-        #     tensorboard_log="./.tb_zipline_env/",
-        # )
+        env = SubprocVecEnv([lambda: ZiplineEnv(**ENV_ARGS, max_steps=TRAIN_STEPS)] * N_ENVS)
+        env = VecNormalize(env, norm_obs=True, norm_reward=True,
+            clip_obs=10.)
 
         model = PPO2(
             MlpPolicy, 
@@ -73,36 +67,36 @@ if __name__ == "__main__":
         )
 
         checkpoint_callback = CheckpointCallback(
-            save_freq=10000, 
+            save_freq=int(1e6), 
             save_path='./.checkpoints/',
-            name_prefix='ppo2_zipline_sortino',
+            name_prefix=SAVE_PATH,
+            vec_norm=env
         )
 
-        model.learn(total_timesteps=1000000, callback=checkpoint_callback)
+        model.learn(total_timesteps=int(10e6), callback=checkpoint_callback)
         model.save(SAVE_PATH)
-        # env.save(STATS_PATH)
+        env.save(STATS_PATH)
 
     # evaluate
-    # model = PPO2.load(SAVE_PATH)
-    model = SAC.load(SAVE_PATH)
+    model = PPO2.load(SAVE_PATH)
 
     env = DummyVecEnv([lambda: ZiplineEnv(**ENV_ARGS, 
         max_steps=512, 
         do_record=True
     )] * N_ENVS)
-    model.set_env(env)
 
     if os.path.isfile(STATS_PATH):
         env = VecNormalize.load(STATS_PATH, env)
         env.training = False
         env.norm_reward = False
 
+    model.set_env(env)
     obs = env.reset()
 
     for i in range(1000):
         action, _states = model.predict(obs)
         obs, rewards, done, info = env.step(action)
-        if i == 256:
+        if i == TRAIN_STEPS:
             print("--- Live trade starts at")
             print(rewards[0])
         env.env_method("render", indices=[0])
